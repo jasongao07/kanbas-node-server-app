f1da5fcea15bee6556e7b048d3a14b98bdaed1a9
jasongao07 <gao.ja@northeastern.edu>
2024-04-16 22:57:58 -0400

    a6

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+module.exports ={
+  "semi": false,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "useTabs": false,
+  "printWidth": 88,
+  "arrowParens": "always",
+  "trailingComma": "all",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "jsxSingleQuote": false,
+  "tsxSingleQuote": true,
+  "quoteProps": "as-needed",
+  "endOfLine": "auto"
+}
